794dff5ab13a721d3d684f3e882a8161a27a3b89
向一郡 <2271027800@qq.com>
2025-03-20 15:27:30 +0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'experience', 'publications'];
+const section_names = ['home', 'awards', 'experience'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
